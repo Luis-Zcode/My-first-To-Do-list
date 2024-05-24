f558f5dabd392c364a41137734e3c261096e8c8b
Luis_GT <luiszcode23@gmail.com>
2024-05-24 16:27:05 -0600

    Limpiando index.html e index.js

--- index.js
+++ index.js
@@ -8,26 +8,6 @@ let value = ''
 creaTarea.addEventListener('change', (e) => {
     value = e.target.value
  })
-
-// const crearTarea = () => {
-//     const div = document.createElement('div')
-//     div.classList.add('form-check')
-//     const input = document.createElement('input')
-//     input.type = 'checkbox'
-//     input.id = 'dos'
-//     input.classList.add('form-check-input')
-//     const label = document.createElement('label')
-//     label.classList.add('form-check-label')
-//     const parrafo = document.createElement('p')
-//     parrafo.id = 'textDos'
-//     parrafo.textContent = value
-//     div.insertAdjacentElement('afterbegin', label)
-//     div.insertAdjacentElement('afterbegin', input)
-//     label.insertAdjacentElement('afterbegin', parrafo)
-//     const  tareas = document.querySelector('#tareas')
-
-//     tareas.insertAdjacentElement('afterbegin', div)
-// }
 const tareaUno = document.querySelector('#uno')
 const action = () => {
     const textUno = document.querySelector('#textUno')
@@ -35,9 +15,7 @@ const action = () => {
 }
 
 
-
 let contador = 0
-
 const crearTareita = () => {
    
     if(value.trim().length == 0){
@@ -66,21 +44,17 @@ const crearTareita = () => {
  </div>
     `
  
-
     creaTarea.value = ''
     value = ''
 
     const tareas = document.querySelector('#tareas')
-    tareas.insertAdjacentHTML('afterbegin', div)
-    
+    tareas.insertAdjacentHTML('afterbegin', div) 
 
     const CheckboxCreado = document.querySelector(`#tarea-${contador}`)
     const ParrafoCreado = document.querySelector(`#parrafo-${contador}`)
     const Color = document.querySelector(`#color`)
     const padre = document.querySelector(`#padre-${contador}`)
    
-
-
     CheckboxCreado.addEventListener('click', () => {
         ParrafoCreado.classList.toggle('text-decoration-line-through')
 
@@ -98,29 +72,7 @@ const crearTareita = () => {
         padre.remove()
     })
 }
-
 }
 
 tareaUno.addEventListener('click', action)
 btnTarea.addEventListener('click', crearTareita)
-
-
-
-//Mover elemento
-// const element = document.querySelector(".element");
-
-// const keyframes = [
-//   { transform: "translate(0, 0)" },
-//   { transform: "translate(900px, 0)" },
-//   { transform: "translate(600px, 0px)" },
-//   { transform: "translate(0, 0px)" }
-// ];
-
-// element.animate(keyframes, 10000);
-
-/* <div class="form-check">
-    <input id="uno" class="form-check-input" type="checkbox" value="" >
-    <label class="form-check-label" for="defaultCheck1">
-     <p id="textUno">Tengo que lavar maniana a las 8:00</p>
-    </label>
-    </div> */
